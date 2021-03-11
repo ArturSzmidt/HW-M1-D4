45c9b3f56dd7eb3e897ba145c776635c02861be2
ArturSzmidt <80460700+ArturSzmidt@users.noreply.github.com>
2021-03-11 16:40:33 +0100

    Update d4.js

--- d4.js
+++ d4.js
@@ -7,33 +7,93 @@ ASSIGNMENT RULES
 */
 
 /* EXERCISE 1
-Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
+Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of 
+the associated rectangle.
 */
 
+// const area = function(l1,l2) {
+//   let total = l1 * l2
+//   return total
+// } 
+// const areaSum = area(12 , 5 )
+// console.log(areaSum)
+
 /* EXERCISE 2
-Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
+Write a function "crazySum" which receives two integers.
+ It should return the sum of those two values, 
+ but if the two values are the same then it should return their sum multiplied by 3.
 */
+
+const crazySum = function(numberOne , numberTwo){
+            
+    if(numberOne === numberTwo){
+       
+        let total = (numberOne + numberTwo) * 3
+        return total
+    } 
+    
+    else{
+         let total = numberOne + numberTwo
+        return total
+    }
+}
+const crazySumTotal = crazySum(13 , 12)
+console.log(crazySumTotal)
+
+
+
 
 /* EXERCISE 3
 Write a function "crazyDiff" that computes the absolute difference between a given number and 19. 
 It should return triple their absolute difference if the given number is greater than 19.
 */
 
+const crazyDiff= function(n) {
+    const value =  Math.abs(n-19) 
+    
+    if (value >19){
+       
+        return value *3
+    } else {
+        return value
+    }
+    
+}
+
+
+ const sumFunction = crazyDiff(5)
+ console.log(sumFunction)
 /* EXERCISE 4
 Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
 */
 
-/* WRITE YOUR CODE HERE */
+const boundary = function(n) {
+if((n>20 && n<=100) || n===400) {
+    return true
 
+} else {
+    return false
+}
+}
+const boundaryResult = boundary(25)
+console.log(boundaryResult)
 /* EXERCISE 5
 Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR CODE HERE */
-
+const strivify = function(str) {
+if(str.startsWith('Strive')) {
+    return str
+} else {
+    return 'Strive ' + str
+}
+}
+const strivifyResult = strivify('whatever')
+console.log(strivifyResult)
 /* EXERCISE 6
-Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
+Write a function "check3and7" which accepts 
+a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
 
